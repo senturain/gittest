6cfbd5e5ce95b2442809c684d22cd5ad544bf257
Chengyu Sun <csun@calstatela.edu>
2025-10-30 12:36:04 -0700

    Added a model attribute

--- src/main/java/edu/csula/cysun/gittest/IndexController.java
+++ src/main/java/edu/csula/cysun/gittest/IndexController.java
@@ -1,13 +1,15 @@
 package edu.csula.cysun.gittest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    String index(){
+    String index(Model model){
+        model.addAttribute("title", "Gittest");
         return "index";
     }
 }
