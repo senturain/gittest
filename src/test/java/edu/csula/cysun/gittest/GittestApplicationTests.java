048724566d46b294244c59ca51846e6a3b25f6a9
Chengyu Sun <csun@calstatela.edu>
2025-10-30 12:25:24 -0700

    Initial commit

---
+++ src/test/java/edu/csula/cysun/gittest/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.csula.cysun.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
