9cf0f3f90074e6162bb7305996bacbfb649857f5
Chengyu Sun <csun@calstatela.edu>
2025-10-30 12:30:55 -0700

    Added a view

--- src/main/java/edu/csula/cysun/gittest/IndexController.java
+++ src/main/java/edu/csula/cysun/gittest/IndexController.java
@@ -1,7 +1,13 @@
 package edu.csula.cysun.gittest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    String index(){
+        return "index";
+    }
 }
